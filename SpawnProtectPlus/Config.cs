c2c8413523533761d7283d463ac511b4103fd55d
ShirakamiRain <14082141+shirakamirain@user.noreply.gitee.com>
2024-08-24 21:34:38 +0800

    添加项目文件。

---
+++ SpawnProtectPlus/Config.cs
@@ -0,0 +1,42 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Features.Roles;
+using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpawnProtectPlus
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+    }
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "SpawnProtectPlus";
+
+        /// <inheritdoc>
+        public override string Prefix => "SProtect+";
+
+        /// <inheritdoc>
+        public override string Author => "ShirakamiAme";
+
+        /// <inheritdoc>
+        public override Version Version => new Version(0,0,1);
+        private EventHandlers eventHandlers;
+        public override void OnEnabled()
+        {
+            eventHandlers = new EventHandlers();
+            Exiled.Events.Handlers.Player.Hurting += eventHandlers.OnHurting;//Subscribe Player's HurtingEvents.订阅玩家受伤的事件
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+        }
+
+    }
+}
---
+++ SpawnProtectPlus/EventHandlers.cs
@@ -0,0 +1,26 @@
+﻿using CustomPlayerEffects;
+using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using Exiled.Events.EventArgs.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpawnProtectPlus
+{
+    internal class EventHandlers
+    {
+        public void OnHurting(HurtingEventArgs ev)
+        {
+            foreach (StatusEffectBase statusEffectBase in ev.Player.ActiveEffects.ToList<StatusEffectBase>())//Traversal player's ActiveEffects.遍历玩家的已激活效果
+            {
+                if (statusEffectBase.GetEffectType() == EffectType.SpawnProtected)//If player is SpawnProtected.如果玩家有出生保护
+                {
+                    ev.IsAllowed = false;//Then Nullify the damage.直接把伤害无效
+                }
+            }
+        }
+    }
+}
